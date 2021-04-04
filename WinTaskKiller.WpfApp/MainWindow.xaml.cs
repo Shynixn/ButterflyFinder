5a8c4fce22469a57539c627418db872bf56764be
BuildTools <unconfigured@null.spigotmc.org>
2021-04-04 13:17:15 +0200

    Completed app.

--- WinTaskKiller.WpfApp/MainWindow.xaml.cs
+++ WinTaskKiller.WpfApp/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 using MiniMvvm.Framework.Contracts;
+using KeyEventArgs = System.Windows.Input.KeyEventArgs;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace WinTaskKiller.WpfApp
 {
@@ -11,6 +16,13 @@ namespace WinTaskKiller.WpfApp
             InitializeComponent();
             Topmost = true;
             WindowState = WindowState.Maximized;
+
+            if (IsAlreadyRunning())
+            {
+                MessageBox.Show("Another instance of the app is already running.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
         }
 
         private async void OnWindowLoaded(object sender, RoutedEventArgs e)
@@ -34,6 +46,27 @@ namespace WinTaskKiller.WpfApp
             {
                 Hide();
             }
+        }
+
+        private bool IsAlreadyRunning()
+        {
+            try
+            {
+                Process curr = Process.GetCurrentProcess();
+                Process[] procs = Process.GetProcessesByName(curr.ProcessName);
+                foreach (Process p in procs)
+                {
+                    if ((p.Id != curr.Id) &&
+                        (p.MainModule.FileName == curr.MainModule.FileName))
+                        return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+            return false;
         }
     }
 }
